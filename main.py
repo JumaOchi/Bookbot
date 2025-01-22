20cda2cb7a4bbdf3f01f5f4b9a58e9abecea97f5
JumaOchi <ochiengjuma95@gmail.com>
2025-01-23 02:42:48 +0300

    added character sort couunt and edited main

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ def main() :
     with open(path_to_file) as f :
         file_contents = f.read()
 
-    #print(file_contents)
+    print("--- Begin report of books/frankenstein.txt ---")
     count = number_of_words(file_contents)
-    chara = character_count(file_contents)
-    print(chara)
+    print(f'{count} words found in the document\n')
+    chara = sort_character_count(file_contents)
+    for i in chara :
+        for key, value in i.items():
+            print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
+
+    
+
 
 def number_of_words(file) :
     return len(file.split())
+
 
 def character_count(file):
     # Convert the string to lowercase
@@ -23,6 +31,13 @@ def character_count(file):
         else:
             characters[letter] = 1
     return characters
+
+def sort_character_count(file) :
+    character_dict = character_count(file)
+
+    character_list = [{char:count} for char, count in character_dict.items() if char.isalpha()]
+    character_list.sort(key=lambda x: list(x.values())[0], reverse=True)
+    return character_list
 
 
 
